906654196026b0d3a55f7260727abb5af5b0d1e7
Deepak Emani <deepakemani@gmail.com>
2018-09-14 07:09:54 +0100

    first commit

---
+++ src/main/java/com/deepak/springbootreactiveexample1/SpringBootReactiveExample1Application.java
@@ -0,0 +1,12 @@
+package com.deepak.springbootreactiveexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveExample1Application.class, args);
+	}
+}
